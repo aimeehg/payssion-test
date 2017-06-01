d12b310f4bc9f48bf4d6a36f13a4ce5fa1b4ebfa
Aimee <zvok59@gmail.com>
2017-06-01 11:27:31 -0500

    aaaa

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
         <title>Payssion test</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
     <body>
+    <b>Choose payment method:</b>
+    <div class="row">
+    <div class="col-sm-4"><img src="images/alipay.png"></div>
+    <div class="col-sm-4"><img src="images/unionpay.png"></div>
+    <div class="col-sm-4"><img src="images/tenpay.png"></div>
+    </div>
+    <br>
     <form name="payssion_payment" action="https://www.payssion.com/payment/create.html" method="post">
     <input type="hidden" name="api_key" value="6eac014c862c695c">
     <input type="hidden" name="api_sig" value="98147c9bf749c14bbdba00744d08a7a4">
