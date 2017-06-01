3a2f9ca3faa333cc0a9238c4341942ea92b22151
Aimee <zvok59@gmail.com>
2017-06-01 10:48:43 -0500

    aa

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<a class='payssion-button pay_now_large' data-key='8205d58fb63f10e7'>
-<i class='fa fa-shopping-cart'></i>&nbsp;Pay Via Payssion</a>
-<script src='https://www.payssion.com/static/checkout/button.js' type='text/javascript'>
-</script> 
+<form name="payssion_payment" action="http://sandbox.payssion.com/payment/create.html" method="post">
+    <input type="hidden" name="api_key" value="e22c0ee69230ca35">
+    <input type="hidden" name="api_sig" value="f260d9d2d1edecc091a239695a007da2">
+    <input type="hidden" name="pm_id" value="alipay_cn">
+    <input type="hidden" name="order_id" value="123">
+    <input type="hidden" name="description" value="Charge for test">
+    <input type="hidden" name="amount" value="1.00">
+    <input type="hidden" name="currency" value="USD">
+    <input type="hidden" name="return_url" value="http://sandbox.payssion.com/demo/afterpayment">
+    <input type ="submit" value ="Submit">
+</form>
