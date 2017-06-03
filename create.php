7f079705bd4e5bcbed7f8a30a2479e03a457195f
Aimee <zvok59@gmail.com>
2017-06-03 17:14:37 -0500

    r

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once("../lib/PayssionClient.php");
+require_once("./lib/PayssionClient.php");
 
 //$payssion = new PayssionClient('your api key', 'your secretkey');
 //please uncomment the following if you use sandbox api_key
