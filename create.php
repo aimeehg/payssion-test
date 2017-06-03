2947d04dd6de009152208c3f63557a67d32d9a45
Aimee <zvok59@gmail.com>
2017-06-03 17:13:43 -0500

    fff

--- create.php
+++ create.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once("../lib/PayssionClient.php");
 
 //$payssion = new PayssionClient('your api key', 'your secretkey');
