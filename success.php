9316b597e89eb8edaafdd50f45be242aada494dc
Aimee <zvok59@gmail.com>
2017-06-03 19:20:27 -0500

    s

--- success.php
+++ success.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once("./lib/PayssionClient.php");
 //$payssion = new PayssionClient('your api key', 'your secretkey');
 //please uncomment the following if you use sandbox api_key
 $payssion = new PayssionClient('6eac014c862c695c', '98147c9bf749c14bbdba00744d08a7a4');
